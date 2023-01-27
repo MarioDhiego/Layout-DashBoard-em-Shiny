56def7dd423d51fee0a41a20378a7d9fff689cb5
MarioDhiego <mario.valente@detran.pa.gov.br>
2023-01-27 05:30:39 -0300

    incluuir imagens

--- Layout_Shiny.R
+++ Layout_Shiny.R
@@ -7,6 +7,7 @@ library(shinydashboard)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
+library(sf)
 library(plotly)
 library(DT)
 library(data.table)
@@ -14,6 +15,8 @@ library(reactable)
 library(ggcorrplot)
 library(htmltools)
 library(htmlwidgets)
+library(markdown)
+library(rmarkdown)
 library(fresh)
 
 
@@ -34,22 +37,26 @@ library(fresh)
 
 
 
+
 # Criação dos Menus
 ui <- dashboardPage(title= "Dasboard Acidentes de Transito",skin="blue",
-dashboardHeader(title="Anuário Estatístico de Òbito por Acidentes de Trânsito",
-                titleWidth=650,
-                tags$li(class="dropdown",tags$a(href="https://www.detran.pa.gov.br",icon("linkedin"), "Detran",target="_blank")),tags$li(class="dropdown",tags$a(href="https://github.com/MarioDhiego",icon("github"), "Author",target="_blank")),
+dashboardHeader(title="ANUÁRIO ESTATÍSTICO DE ÓBITO POR ACIDENTES DE TRÂNSITO",
+                titleWidth=700,
+                tags$li(class="dropdown",tags$a(href="https://www.detran.pa.gov.br",icon("linkedin"),"DETRAN-PA",target="_blank")),tags$li(class="dropdown",tags$a(href="https://github.com/MarioDhiego",icon("github"), "AUTHOR",target="_blank")),
                 dropdownMenu(type="messages"),
                 dropdownMenu(type="notifications"),
                 dropdownMenu(type="tasks")
 ),
 dashboardSidebar(
   sidebarMenu(
-    menuItem("ANUÁRIO"                     ,tabName="about1",icon=icon("mortar-board")),
+    menuItem("ANUÁRIO"                       ,tabName="about1",icon=icon("address-card"),
+            menuSubItem("Sobre Anuário"      ,tabName="sobre1"),
+            menuSubItem("Pareamento de Dados",tabName="pareamento1") 
+             ),
     menuItem("MICRODADOS"                  ,tabName="banco1",icon=icon("database"),
-              menuSubItem("Base de Dados"   ,tabName="base1"),
-              menuSubItem("Fonte de Dados"  ,tabName="fonte1"),
-              menuSubItem("Medidas"         ,tabName="medi1")),
+              menuSubItem("Base de Dados"  ,tabName="base1"),
+              menuSubItem("Fonte de Dados" ,tabName="fonte1"),
+              menuSubItem("Medidas"        ,tabName="medi1")),
     menuItem("SOCIOECONÔMICO"              ,tabName="socio1",icon=icon("male"),
              menuSubItem("Gênero"          ,tabName="genero1"),
              menuSubItem("Faixa Etaria"    ,tabName="idade1"),
@@ -69,8 +76,8 @@ dashboardSidebar(
            menuSubItem("Ano"               ,tabName="ano1"),
            menuSubItem("Turno"             ,tabName="turno1")),
   menuItem("CUSTO HOSPITALAR"              ,tabName="custo1",icon = icon("hospital"),
-           menuSubItem("Dias de Internacao"  ,tabName="dias1"),
-           menuSubItem("Custo de internacao" ,tabName="custo2")),
+           menuSubItem("Dias de Internacao" ,tabName="dias1"),
+           menuSubItem("Custo de internacao",tabName="custo2")),
   menuItem("MAPAS"                         ,tabName="map1", icon=icon("globe"),
            menuSubItem("Belém"             ,tabName="bel1"),
            menuSubItem("Municípios"        ,tabName="munic1"))
@@ -79,14 +86,20 @@ dashboardSidebar(
 dashboardBody(
 #  use_theme(meu_tema),
   tabItems(
-    tabItem(tabName = "about1",
+    tabItem(tabName="sobre1",
             fluidRow(
-              tags$iframe(src = './about1.html',
-                          width='100%',height='800px',
-                          frameborder=0,scrolling='auto'
-              )
+              column(width = 8, 
+              tags$img(src="crime.jpeg",width =600,height = 400),
+              tags$br() , 
+              tags$a("Photo by Asdecom"),align="left"),
+              column(width = 4, tags$br(),
+              tags$p("Anuário Estatístico de Óbito por Acidente de Trânsito do Detran-Pa, é um catalogo que engloba dados sobre as características das vítimas fatais, cujo resultado é um processo de gestão e integração de multiplas bases de dados, utilizando o Método Probabilistico de Relacionamento de Registros desenvolvido por Fellegi e Sunter(1969)."),
+              tags$p("Para criação do Anuário em formato Web, foi criado um script em Linguagem de Programação R-PROJECT versão 4.2.2 e um Ambiente de Desenvolvimento Integrado(IDE) chamado Rstudio versão 1.4.1.7 com uso de vários pacotes, para o ambiente Windows."),
+              tags$p("O Anuário foi construído em Alinhamento ao Manual de Gestão do RENAEST (Resolução do CONTRAN Nº808/2020), utilizando metodologias factíveis com estatísticas de trânsito padronizada para todo o Território Nacional, e aos Objetivos de Desenvolvimento Sustentáveis (Resolução da ONU Nº70/2015).")
+            )
             )
             ),
+    tabItem(tabName="pareamento1"),
     tabItem(tabName="genero1",
             fluidRow(
               box(width=12,
